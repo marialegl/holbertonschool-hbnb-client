b4b1ae679d62585b6251f60343b352b92c73e457
marialegl <psico.marialegl@gmail.com>
2024-08-01 21:55:48 -0500

    advance 1

--- 03-frontend/base_files/scripts.js
+++ 03-frontend/base_files/scripts.js
@@ -1,10 +1,5 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
-  const loginForm = document.getElementById('login-form');
+    const loginForm = document.getElementById('login-form');
 
   if (loginForm) {
       loginForm.addEventListener('submit', async (event) => {
--- 03-frontend/mock-api/app.py
+++ 03-frontend/mock-api/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_cors import CORS
 import json
