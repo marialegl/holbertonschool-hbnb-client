47d236faddbc155a30f545a50c66ea24f3ada487
NicowaxDelux <nycko111995@gmail.com>
2024-08-05 19:31:04 -0500

    update

--- 03-frontend/base_files/scripts.js
+++ 03-frontend/base_files/scripts.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
-    const loginForm = document.getElementById('login-form');
 
   if (loginForm) {
       loginForm.addEventListener('submit', async (event) => {
