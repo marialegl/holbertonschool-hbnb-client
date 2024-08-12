acab1db54c7b9977dd3fb62158d42478b94fbdf1
marialegl <psico.marialegl@gmail.com>
2024-08-12 10:18:56 -0500

    branch movement update

--- frontend/mock-api/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecretkey')
-    JWT_ACCESS_TOKEN_EXPIRES = False
